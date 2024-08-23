7f8709bd717e6069b307f7c35cfe25f6677ee9ed
yukinko05 <grey.sora.35@gmail.com>
2024-08-23 14:17:27 +0900

    HomeコンポーネントのStyle調整

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -6,7 +6,7 @@ import './Home.css';
 type Threads = {
   id: number;
   title: string;
-}
+};
 
 export default function Home() {
   const [threads, setThreads] = useState<Threads[]>([]);
@@ -16,20 +16,20 @@ export default function Home() {
       .then(res => res.json())
       .then(data => setThreads(data))
       .catch(error => {
-        console.log("スレッドのデータを取得できません", error)
+        console.log("スレッドのデータを取得できません。", error)
       })
   }, []);
-  console.log(threads)
+
   return (
     <>
       <Header>
-        <Link to="/threads/new" className='threadCreateLink' >スレッドをたてる</Link>
+        <Link to="/threads/new" className="threadCreateLink" >スレッドをたてる</Link>
       </Header >
-      <section className='threadContainer'>
-        <h1>新着スレッド</h1>
+      <section className="threadContainer">
+        <h1 className="pageTitle">New Threads</h1>
         <ul>
           {threads.map(thread => (
-            <li className='threadCord' key={thread.id}>{thread.title}</li>
+            <li className="threadListItem" key={thread.id}>{thread.title}</li>
           ))}
         </ul>
       </section>
